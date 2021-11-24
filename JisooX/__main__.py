91c032c198900b0ee1ef9616d963e04ccefb8431
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-11-24 09:05:39 +0800

    Update __main__.py

--- JisooX/__main__.py
+++ JisooX/__main__.py
@@ -193,8 +193,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="[► Help ◄]",callback_data="help_back"),InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/helzrip")]]
-    keyboard += [[InlineKeyboardButton(text="[► Source ◄]",url="https://github.com/sophiashirashaki"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="[► Help ◄]",callback_data="help_back"),InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/Shinhwagyu")]]
+    keyboard += [[InlineKeyboardButton(text="[► Source ◄]",url="https://github.com/SS404"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
@@ -295,7 +295,7 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="[► Help ◄]",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/helzrip")]]))
+                                                [InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/Shinhwagyu")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
